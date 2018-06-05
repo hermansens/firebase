0461d38d899650ecbea3446059ab38364c18fd2e
Søren Hermansen <sorenhermansen@jonas.kea.local>
2018-06-05 10:53:41 +0200

    delete

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const form = document.querySelector("form");
 const template = document.querySelector("#noteTemplate").content;
 const app = document.querySelector("#app");
 
+
+// add new notes
 form.addEventListener("submit", (e)=> {
     e.preventDefault();
 
@@ -23,7 +25,19 @@ database.ref("notes/").on("child_added", (snapshot)=>{
     const data = snapshot.val();
 
     const clone = template.cloneNode(true);
+    clone.querySelector("article").dataset.key=key;
     clone.querySelector("h1").textContent = data.header;
     clone.querySelector("div").textContent = data.description;
+    clone.querySelector("button.delete").addEventListener("click", e=>{
+        database.ref("notes/"+key).remove();
+    });
     app.appendChild(clone);
+});
+
+//listen for removal of data
+
+database.ref("notes/").on("child_removed", snapshot=>{
+    const key = snapshot.key;
+    let el = document.querySelector(`article[data-key=${key}]`);
+    el.remove();
 });
